044ff4aa329b9bb1272797b539b7a558a58a3d59
izumi <kizumi916@gmail.com>
2020-06-05 12:49:30 +0900

    リファクタリング

--- js/main.js
+++ js/main.js
@@ -4,28 +4,7 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 6);
-    // btn.textContent = n;
-
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '吉';
-        break;
-      case 2:
-        btn.textContent = '中吉';
-        break;
-      case 3:
-        btn.textContent = '小吉';
-        break;
-      case 4:
-        btn.textContent = '末吉';
-        break;
-      case 5:
-        btn.textContent = '凶';
-        break;
-    }
+    const results = ['大吉', '吉', '中吉', '小吉', '末吉', '凶'];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
   });
 }
