d615b8129f33569a8f1163db1d9584cbd14cc894
izumi <kizumi916@gmail.com>
2020-06-06 20:50:18 +0900

    確率変更

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '吉', '中吉', '小吉', '末吉', '凶'];
+    const results = ['大吉', '吉', 'きち', '中吉', '中吉', '小吉', '小吉', '末吉', '末吉', '凶'];
     btn.textContent = results[Math.floor(Math.random() * results.length)];
   });
 }
